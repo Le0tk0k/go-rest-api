6a896d93bc4a32f681095be12746ac35a1bdaf19
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-01 17:58:12 +0900

    add datastore/db.go

--- server.go
+++ server.go
@@ -3,11 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/Le0tk0k/go-rest-api/infrastructure/datastore"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	conn := datastore.NewMySqlDB()
+	defer conn.Close()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
