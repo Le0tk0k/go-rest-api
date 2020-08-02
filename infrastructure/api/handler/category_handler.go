9ee4c141821a09df5cafda042d2cc54dfff53a78
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-02 09:52:10 +0900

    update category_handler

---
+++ infrastructure/api/handler/category_handler.go
@@ -0,0 +1,70 @@
+package handler
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/Le0tk0k/go-rest-api/domain/model"
+	"github.com/Le0tk0k/go-rest-api/usecase/repository"
+	"github.com/labstack/echo"
+)
+
+type categoryHandler struct {
+	categoryRepository repository.CategoryRepository
+}
+
+type CategoryHandler interface {
+	CreateCategory(c echo.Context)
+	GetCategories(c echo.Context)
+	GetCategory(c echo.Context)
+	UpdateCategory(c echo.Context)
+	DeleteCategory(c echo.Context)
+}
+
+func NewCategoryHandler(cR repository.CategoryRepository) CategoryHandler {
+	return &categoryHandler{categoryRepository: cR}
+}
+
+func (cH *categoryHandler) CreateCategory(c echo.Context) {
+	cg = &model.Category{}
+	c.Bind(cg)
+	cH.categoryRepository.Store(cg)
+
+	c.JSON(http.StatusOK, cg)
+}
+
+func (cH *categoryHandler) GetCategories(c echo.Context) {
+	cg, err := cH.categoryRepository.FindAll()
+
+	c.JSON(http.StatusOK, cg)
+}
+
+func (cH *categoryHandler) GetCategory(c echo.Context) {
+	idString := c.Param("id")
+	id, err := strconv.Atoi(idString)
+
+	cg, err := cH.categoryRepository.FindByID(id)
+
+	c.JSON(http.StatusOK, cg)
+}
+
+func (cH *categoryHandler) UpdateCategory(c echo.Context) {
+	idString := c.Param("id")
+	id, err := strconv.Atoi(idString)
+
+	cg := &model.Category{ID: id}
+
+	c.Bind(cg)
+	cH.categoryRepository.Update(cg)
+
+	c.JSON(http.StatusOK, cg)
+}
+
+func (cH *categoryHandler) DeleteCategory(c echo.Context) {
+	idString := c.Param("id")
+	id, err := strconv.Atoi(idString)
+
+	cH.categoryRepository.Delete(&model.Category{ID: id})
+
+	c.JSON(http.StatusNoContent, echo.H{"message": "ok"})
+}
