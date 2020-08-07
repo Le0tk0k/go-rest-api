a1e449db0107ab00666e779a4cd0403757e54c00
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-07 14:16:04 +0900

    change the structure of Article

--- domain/model/article.go
+++ domain/model/article.go
@@ -6,6 +6,7 @@ type Article struct {
 	ID        int       `gorm:"primary_key" json:"id"`
 	Title     string    `json:"title" validate:"required"`
 	Url       string    `json:"url" validate:"required"`
+	Category  Category  `json:"category`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 }
