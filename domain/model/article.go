aed20a14aa1ce6a71186e47bf610bed1da08db57
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-08 21:04:42 +0900

    update domain

--- domain/model/article.go
+++ domain/model/article.go
@@ -4,9 +4,9 @@ import "time"
 
 type Article struct {
 	ID        int       `gorm:"primary_key" json:"id"`
-	Title     string    `json:"title" validate:"required"`
-	Url       string    `json:"url" validate:"required"`
-	Category  Category  `json:"category`
+	Title     string    `json:"title"`
+	Url       string    `json:"url"`
+	Category  *Category `json:"category`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 }
--- domain/model/category.go
+++ domain/model/category.go
@@ -4,7 +4,7 @@ import "time"
 
 type Category struct {
 	ID        int       `gorm:"primary_key" json:"id`
-	Name      string    `json:"name" validate:"required"`
+	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 }
