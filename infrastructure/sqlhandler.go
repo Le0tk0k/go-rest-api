79d592a9b7ac085ca4468ef32bb86ec6accc85b6
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-09 11:30:14 +0900

    add sqlhandler.go

---
+++ infrastructure/sqlhandler.go
@@ -0,0 +1,35 @@
+package datastore
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func NewMySqlDb() *gorm.DB {
+
+	connectionString := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		"user",
+		"password",
+		"db",
+		"3306",
+		"go_rest_api",
+	)
+
+	conn, err := gorm.Open("mysql", connectionString)
+	if err != nil {
+		panic(err)
+	}
+
+	err = conn.DB().Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	conn.LogMode(true)
+	conn.Set("gorm:table_options", "ENGINE=InnoDB")
+
+	return conn
+}
