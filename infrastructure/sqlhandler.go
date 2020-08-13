13892ffa6b0e5cb76d36bc2b9f97023e6a2e32ab
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-13 17:00:00 +0900

    update sqlhandler.go

--- infrastructure/sqlhandler.go
+++ infrastructure/sqlhandler.go
@@ -3,6 +3,7 @@ package infrastructure
 import (
 	"fmt"
 
+	"github.com/Le0tk0k/go-rest-api/interfaces/database"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -11,7 +12,7 @@ type SqlHandler struct {
 	Conn *gorm.DB
 }
 
-func NewMySqlDb() *SqlHandler {
+func NewMySqlDb() database.SqlHandler {
 
 	connectionString := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
@@ -39,4 +40,36 @@ func NewMySqlDb() *SqlHandler {
 	sqlHandler.Conn = conn
 
 	return sqlHandler
+}
+
+func (handler *SqlHandler) Find(out interface{}, where ...interface{}) *gorm.DB {
+	return handler.Conn.Find(out, where...)
+}
+
+func (handler *SqlHandler) Exec(sql string, values ...interface{}) *gorm.DB {
+	return handler.Conn.Exec(sql, values...)
+}
+
+func (handler *SqlHandler) First(out interface{}, where ...interface{}) *gorm.DB {
+	return handler.Conn.First(out, where...)
+}
+
+func (handler *SqlHandler) Raw(sql string, values ...interface{}) *gorm.DB {
+	return handler.Conn.Raw(sql, values...)
+}
+
+func (handler *SqlHandler) Create(value interface{}) *gorm.DB {
+	return handler.Conn.Create(value)
+}
+
+func (handler *SqlHandler) Save(value interface{}) *gorm.DB {
+	return handler.Conn.Save(value)
+}
+
+func (handler *SqlHandler) Delete(value interface{}) *gorm.DB {
+	return handler.Conn.Delete(value)
+}
+
+func (handler *SqlHandler) Where(query interface{}, args ...interface{}) *gorm.DB {
+	return handler.Conn.Where(query, args...)
 }
