da9b78379d62705807813dc53b2b4158e19a4387
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-19 17:08:53 +0900

    udpate controller

--- interfaces/controllers/article_controller.go
+++ interfaces/controllers/article_controller.go
@@ -4,95 +4,98 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/Le0tk0k/go-rest-api/usecase/repository"
+	"github.com/Le0tk0k/go-rest-api/domain"
+	"github.com/Le0tk0k/go-rest-api/interfaces/database"
+
+	"github.com/Le0tk0k/go-rest-api/usecase"
+
 	"github.com/labstack/echo"
 )
 
-type articleHandler struct {
-	articleRepository  repository.ArticleRepository
-	categoryRepository repository.CategoryRepository
+type ArticleController struct {
+	Interactor usecase.ArticleInteractor
 }
 
-type ArticleHandler interface {
-	CreateArticle(c echo.Context) error
-	GetArticles(c echo.Context) error
-	GetArticle(c echo.Context) error
-	UpdateArticle(c echo.Context) error
-	DeleteArticle(c echo.Context) error
+func NewArticleController(sqlHandler database.SqlHandler) *ArticleController {
+	return &ArticleController{
+		Interactor: usecase.ArticleInteractor{
+			ArticleRepository: &database.ArticleRepository{
+				SqlHandler: sqlHandler,
+			},
+		},
+	}
 }
 
-func NewArticleHandler(aR repository.ArticleRepository, cR repository.CategoryRepository) ArticleHandler {
-	return &articleHandler{articleRepository: aR, categoryRepository: cR}
-}
-
-func (aH *articleHandler) CreateArticle(c echo.Context) error {
-	a := &model.Article{}
+func (controller *ArticleController) CreateArticle(c echo.Context) error {
+	a := &domain.Article{}
 	if err := c.Bind(a); err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	if err := aH.articleRepository.Store(a); err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+	article, err := controller.Interactor.Add(*a)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusOK, a)
+	return c.JSON(http.StatusOK, article)
 }
 
-func (aH *articleHandler) GetArticles(c echo.Context) error {
-	a, err := aH.articleRepository.FindAll()
+func (controller *ArticleController) GetArticles(c echo.Context) error {
+	articles, err := controller.Interactor.Articles()
 
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusOK, a)
+	return c.JSON(http.StatusOK, articles)
 }
 
-func (aH *articleHandler) GetArticle(c echo.Context) error {
+func (controller *ArticleController) GetArticle(c echo.Context) error {
 	idString := c.Param("id")
 	id, err := strconv.Atoi(idString)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	a, err := aH.articleRepository.FindByID(id)
+	article, err := controller.Interactor.ArticleByID(id)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusOK, a)
+	return c.JSON(http.StatusOK, article)
 }
 
-func (aH *articleHandler) UpdateArticle(c echo.Context) error {
+func (controller *ArticleController) UpdateArticle(c echo.Context) error {
 	idString := c.Param("id")
 	id, err := strconv.Atoi(idString)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	a := &model.Article{ID: id}
+	article := &domain.Article{ID: id}
 
-	if err := c.Bind(a); err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+	if err := c.Bind(article); err != nil {
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	if err := aH.articleRepository.Update(a); err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+	if err := controller.Interactor.Update(article); err != nil {
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusOK, a)
+	return c.JSON(http.StatusOK, article)
 }
 
-func (aH *articleHandler) DeleteArticle(c echo.Context) error {
+func (controller *ArticleController) DeleteArticle(c echo.Context) error {
 	idString := c.Param("id")
 	id, err := strconv.Atoi(idString)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	if err := aH.articleRepository.Delete(&model.Article{ID: id}); err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+	article := &domain.Article{ID: id}
+	if err := controller.Interactor.DeleteById(article); err != nil {
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusCreated, "success")
+	return c.JSON(http.StatusOK, article)
 }
--- interfaces/controllers/category_controller.go
+++ interfaces/controllers/category_controller.go
@@ -4,94 +4,96 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/Le0tk0k/go-rest-api/usecase/repository"
+	"github.com/Le0tk0k/go-rest-api/domain"
+	"github.com/Le0tk0k/go-rest-api/interfaces/database"
+	"github.com/Le0tk0k/go-rest-api/usecase"
 	"github.com/labstack/echo"
 )
 
-type categoryHandler struct {
-	categoryRepository repository.CategoryRepository
+type CategoryController struct {
+	Interactor usecase.CategoryInteractor
 }
 
-type CategoryHandler interface {
-	CreateCategory(c echo.Context) error
-	GetCategories(c echo.Context) error
-	GetCategory(c echo.Context) error
-	UpdateCategory(c echo.Context) error
-	DeleteCategory(c echo.Context) error
+func NewCategoryController(sqlHandler database.SqlHandler) *CategoryController {
+	return &CategoryController{
+		Interactor: usecase.CategoryInteractor{
+			CategoryRepository: &database.CategoryRepository{
+				SqlHandler: sqlHandler,
+			},
+		},
+	}
 }
 
-func NewCategoryHandler(cR repository.CategoryRepository) CategoryHandler {
-	return &categoryHandler{categoryRepository: cR}
-}
-
-func (cH *categoryHandler) CreateCategory(c echo.Context) error {
-	cg := &model.Category{}
+func (controller *CategoryController) CreateCategory(c echo.Context) error {
+	cg := &domain.Category{}
 	if err := c.Bind(cg); err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	if err := cH.categoryRepository.Store(cg); err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+	category, err := controller.Interactor.Add(*cg)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusOK, cg)
+	return c.JSON(http.StatusOK, category)
 }
 
-func (cH *categoryHandler) GetCategories(c echo.Context) error {
-	cg, err := cH.categoryRepository.FindAll()
+func (controller *CategoryController) GetCategories(c echo.Context) error {
+	categories, err := controller.Interactor.Categories()
 
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusOK, cg)
+	return c.JSON(http.StatusOK, categories)
 }
 
-func (cH *categoryHandler) GetCategory(c echo.Context) error {
+func (controller *CategoryController) GetCategory(c echo.Context) error {
 	idString := c.Param("id")
 	id, err := strconv.Atoi(idString)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	cg, err := cH.categoryRepository.FindByID(id)
+	category, err := controller.Interactor.CategoryByID(id)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusOK, cg)
+	return c.JSON(http.StatusOK, category)
 }
 
-func (cH *categoryHandler) UpdateCategory(c echo.Context) error {
+func (controller *CategoryController) UpdateCategory(c echo.Context) error {
 	idString := c.Param("id")
 	id, err := strconv.Atoi(idString)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	cg := &model.Category{ID: id}
+	category := &domain.Category{ID: id}
 
-	if err := c.Bind(cg); err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+	if err := c.Bind(category); err != nil {
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	if err := cH.categoryRepository.Update(cg); err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+	if err := controller.Interactor.Update(category); err != nil {
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusOK, cg)
+	return c.JSON(http.StatusOK, category)
 }
 
-func (cH *categoryHandler) DeleteCategory(c echo.Context) error {
+func (controller *CategoryController) DeleteCategory(c echo.Context) error {
 	idString := c.Param("id")
 	id, err := strconv.Atoi(idString)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	if err := cH.categoryRepository.Delete(&model.Category{ID: id}); err != nil {
-		return c.JSON(http.StatusBadRequest, model.ResponseError{Message: err.Error()})
+	category := &domain.Category{ID: id}
+	if err := controller.Interactor.DeleteById(cateogyr); err != nil {
+		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusCreated, "success")
+	return c.JSON(http.StatusOK, category)
 }
