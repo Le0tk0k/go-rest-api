4d4813e7667338a18c5edfc3d743323ac31197e3
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-19 17:17:39 +0900

    update router.go

--- infrastructure/router.go
+++ infrastructure/router.go
@@ -1,6 +1,7 @@
 package infrastructure
 
 import (
+	"github.com/Le0tk0k/go-rest-api/interfaces/controllers"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -8,8 +9,23 @@ import (
 func Init() {
 	e := echo.New()
 
+	articleController := controllers.NewArticleController(NewSqlHandler())
+	categoryController := controllers.NewCategoryController(NewSqlHandler())
+
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	e.GET("/articles", func(c echo.Context) error { return articleController.GetArticles(c) })
+	e.GET("/articles/:id", func(c echo.Context) error { return articleController.GetArticle(c) })
+	e.POST("/articles", func(c echo.Context) error { return articleController.CreateArticle(c) })
+	e.PUT("/articles/:id", func(c echo.Context) error { return articleController.UpdateArticle(c) })
+	e.DELETE("/articles/:id", func(c echo.Context) error { return articleController.DeleteArticle(c) })
+
+	e.GET("/categories", func(c echo.Context) error { return categoryController.GetCategories(c) })
+	e.GET("/categories/:id", func(c echo.Context) error { return categoryController.GetCategory(c) })
+	e.POST("/categories", func(c echo.Context) error { return categoryController.CreateCategory(c) })
+	e.PUT("/categories/:id", func(c echo.Context) error { return categoryController.UpdateCategory(c) })
+	e.DELETE("/categories/:id", func(c echo.Context) error { return categoryController.DeleteCategory(c) })
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
--- server.go
+++ server.go
@@ -1,37 +1,7 @@
 package main
 
-import (
-	"github.com/Le0tk0k/go-rest-api/infrastructure/api/handler"
-	"github.com/Le0tk0k/go-rest-api/infrastructure/datastore"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
-)
+import "github.com/Le0tk0k/go-rest-api/infrastructure"
 
 func main() {
-	conn := datastore.NewMySqlDb()
-	defer conn.Close()
-	articleRepo := datastore.NewArticleRepository(conn)
-	categoryRepo := datastore.NewCategoryRepository(conn)
-
-	aH := handler.NewArticleHandler(articleRepo, categoryRepo)
-	cH := handler.NewCategoryHandler(categoryRepo)
-
-	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/articles", aH.GetArticles)
-	e.GET("/articles/:id", aH.GetArticle)
-	e.POST("/articles", aH.CreateArticle)
-	e.PUT("/articles/:id", aH.UpdateArticle)
-	e.DELETE("/articles/:id", aH.DeleteArticle)
-
-	e.GET("/categories", cH.GetCategories)
-	e.GET("/categories/:id", cH.GetCategory)
-	e.POST("/categories", cH.CreateCategory)
-	e.PUT("/categories/:id", cH.UpdateCategory)
-	e.DELETE("/categories/:id", cH.DeleteCategory)
-
-	e.Logger.Fatal(e.Start(":8080"))
+	infrastructure.Init()
 }
