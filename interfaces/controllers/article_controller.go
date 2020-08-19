ddc299e9dcbbd5d68188bf6871f0e19c47e44f48
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-19 17:35:46 +0900

    update article_controller.go

--- interfaces/controllers/article_controller.go
+++ interfaces/controllers/article_controller.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"net/http"
 	"strconv"
 
 	"github.com/Le0tk0k/go-rest-api/domain"
@@ -27,75 +26,64 @@ func NewArticleController(sqlHandler database.SqlHandler) *ArticleController {
 }
 
 func (controller *ArticleController) CreateArticle(c echo.Context) error {
-	a := &domain.Article{}
-	if err := c.Bind(a); err != nil {
-		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
-	}
+	a := domain.Article{}
+	c.Bind(&a)
+	article, err := controller.Interactor.Add(a)
 
-	article, err := controller.Interactor.Add(*a)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
+		c.JSON(500, NewError(err))
+		return
 	}
-
-	return c.JSON(http.StatusOK, article)
+	c.JSON(201, article)
+	return
 }
 
 func (controller *ArticleController) GetArticles(c echo.Context) error {
 	articles, err := controller.Interactor.Articles()
 
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
+		c.JSON(500, NewError(err))
+		return
 	}
-
-	return c.JSON(http.StatusOK, articles)
+	c.JSON(200, articles)
+	return
 }
 
 func (controller *ArticleController) GetArticle(c echo.Context) error {
-	idString := c.Param("id")
-	id, err := strconv.Atoi(idString)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
-	}
-
+	id, _ := strconv.Atoi(c.Param("id"))
 	article, err := controller.Interactor.ArticleByID(id)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
-	}
 
-	return c.JSON(http.StatusOK, article)
+	if err != nil {
+		c.JSON(500, NewError(err))
+		return
+	}
+	c.JSON(200, articles)
+	return
 }
 
 func (controller *ArticleController) UpdateArticle(c echo.Context) error {
-	idString := c.Param("id")
-	id, err := strconv.Atoi(idString)
+	id, _ := strconv.Atoi(c.Param("id"))
+	a := domain.Article{ID: id}
+	c.Bind(&a)
+
+	article, err := controller.Interactor.Update(a)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
+		c.JSON(500, NewError(err))
+		return
 	}
-
-	article := &domain.Article{ID: id}
-
-	if err := c.Bind(article); err != nil {
-		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
-	}
-
-	if err := controller.Interactor.Update(article); err != nil {
-		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
-	}
-
-	return c.JSON(http.StatusOK, article)
+	c.JSON(201, user)
+	return
 }
 
 func (controller *ArticleController) DeleteArticle(c echo.Context) error {
-	idString := c.Param("id")
-	id, err := strconv.Atoi(idString)
+	id, _ := strconv.Atoi(c.Param("id"))
+	article := domain.Article{ID: id}
+
+	err := controller.Interactor.DeleteById(article)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
+		c.JSON(500, NewError(err))
+		return
 	}
-
-	article := &domain.Article{ID: id}
-	if err := controller.Interactor.DeleteById(article); err != nil {
-		return c.JSON(http.StatusBadRequest, domain.ResponseError{Message: err.Error()})
-	}
-
-	return c.JSON(http.StatusOK, article)
+	c.JSON(200, user)
+	return
 }
