415ff310dc6051d2cae5fff240a97f8130eb6eb6
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-19 18:12:32 +0900

    fix typo

--- usecase/article_interactor.go
+++ usecase/article_interactor.go
@@ -27,6 +27,6 @@ func (interactor *ArticleInteractor) Update(a domain.Article) (article domain.Ar
 }
 
 func (interactor *ArticleInteractor) DeleteById(article domain.Article) (err error) {
-	err = interactor.ArticleRepository.DeleteBy(article)
+	err = interactor.ArticleRepository.Delete(article)
 	return
 }
