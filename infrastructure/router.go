84db6049e414ca9009c9f87ca83601ff3bdd5040
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-19 13:29:42 +0900

    add router.go

---
+++ infrastructure/router.go
@@ -0,0 +1,15 @@
+package infrastructure
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func Init() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
