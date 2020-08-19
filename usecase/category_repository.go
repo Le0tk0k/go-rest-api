3d00cea30668edb749000031c51052c4535057da
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-19 13:24:51 +0900

    update category_repository.go

--- usecase/category_repository.go
+++ usecase/category_repository.go
@@ -1,11 +1,11 @@
 package usecase
 
-import "github.com/Le0tk0k/go-rest-api/domain/model"
+import "github.com/Le0tk0k/go-rest-api/domain"
 
 type CategoryRepository interface {
-	FindByID(id int) (*model.Category, error)
-	Store(category *model.Category) error
-	Update(category *model.Category) error
-	Delete(category *model.Category) error
-	FindAll() ([]*model.Category, error)
+	FindByID(id int) (*domain.Category, error)
+	Store(category *domain.Category) error
+	Update(category *domain.Category) error
+	Delete(category *domain.Category) error
+	FindAll() ([]*domain.Category, error)
 }
