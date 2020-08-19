92a8fd11dc58a9c184e5a08038fd551e9d4ab843
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-19 17:56:55 +0900

    update interactor

--- usecase/article_interactor.go
+++ usecase/article_interactor.go
@@ -6,22 +6,22 @@ type ArticleInteractor struct {
 	ArticleRepository ArticleRepository
 }
 
-func (interactor *ArticleInteractor) ArticleById(id int) (article *domain.Article, err error) {
+func (interactor *ArticleInteractor) ArticleById(id int) (article domain.Article, err error) {
 	article, err = interactor.ArticleRepository.FindByID(id)
 	return
 }
 
-func (interactor *ArticleInteractor) Articles() (articles *domain.Article, err error) {
+func (interactor *ArticleInteractor) Articles() (articles domain.Article, err error) {
 	articles, err = interactor.ArticleRepository.FindAll()
 	return
 }
 
-func (interactor *ArticleInteractor) Add(article domain.Article) (article *domain.Article, err error) {
+func (interactor *ArticleInteractor) Add(article domain.Article) (article domain.Article, err error) {
 	article, err = interactor.ArticleRepository.Store(article)
 	return
 }
 
-func (interactor *ArticleInteractor) Update(article domain.Article) (article *domain.Article, err error) {
+func (interactor *ArticleInteractor) Update(article domain.Article) (article domain.Article, err error) {
 	article, err = interactor.ArticleRepository.Update(article)
 	return
 }
--- usecase/category_interactor.go
+++ usecase/category_interactor.go
@@ -6,22 +6,22 @@ type CategoryInteractor struct {
 	CategoryRepository CategoryRepository
 }
 
-func (interactor *CategoryInteractor) CategoryById(id int) (category *domain.Category, err error) {
+func (interactor *CategoryInteractor) CategoryById(id int) (category domain.Category, err error) {
 	category, err = interactor.CategoryRepository.FindByID(id)
 	return
 }
 
-func (interactor *CategoryInteractor) Categories() (categories *domain.Category, err error) {
+func (interactor *CategoryInteractor) Categories() (categories domain.Category, err error) {
 	categories, err = interactor.CategoryRepository.FindAll()
 	return
 }
 
-func (interactor *CategoryInteractor) Add(category domain.Category) (category *domain.Category, err error) {
+func (interactor *CategoryInteractor) Add(category domain.Category) (category domain.Category, err error) {
 	category, err = interactor.CategoryRepository.Store(category)
 	return
 }
 
-func (interactor *CategoryInteractor) Update(category domain.Category) (category *domain.Category, err error) {
+func (interactor *CategoryInteractor) Update(category domain.Category) (category domain.Category, err error) {
 	category, err = interactor.CategoryRepository.Update(category)
 	return
 }
