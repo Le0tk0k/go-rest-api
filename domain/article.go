62d08c051c0bad610be4ad4eeb0354de5bf12593
Leo Takaoka <tlkanb0908@gmail.com>
2020-08-20 20:37:11 +0900

    change scheme

--- domain/article.go
+++ domain/article.go
@@ -6,7 +6,6 @@ type Article struct {
 	ID        int       `gorm:"primary_key" json:"id"`
 	Title     string    `json:"title"`
 	Url       string    `json:"url"`
-	Category  *Category `json:"category"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 }
